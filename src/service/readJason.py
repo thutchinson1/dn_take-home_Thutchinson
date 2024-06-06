0cd7b2a5889c65290101ab6e265e6890879f4b4c
thutchinson1 <titushutchinson79@gmail.com>
2024-06-06 18:14:21 -0400

    read the json file and load the data to a dictionary - 1

--- src/service/readJason.py
+++ src/service/readJason.py
@@ -1,12 +1,20 @@
 # this file loads the json file and adds the values to the object.
 
 import json
+import pandas as pd
 
 # Open the JSON file
 with open("../json/j8mb-icvb.json", "r") as f:
     data = json.load(f)
 
-# Iterate over each dictionary in the list
-for dictionary in data:
-    # Print the items of each dictionary
-    print(dictionary.items())
+# # Iterate over each dictionary in the list
+# for dictionary in data:
+#     # Print the items of each dictionary
+#     print(dictionary.items())
+
+
+# Create a DataFrame from the list of dictionaries
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)

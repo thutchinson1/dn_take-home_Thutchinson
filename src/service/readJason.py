60f2bb7a23485a1bb9ba4b755b393fa6ed32b507
thutchinson1 <titushutchinson79@gmail.com>
2024-06-06 17:32:17 -0400

    read the json file and load the data to a dictionary

---
+++ src/service/readJason.py
@@ -0,0 +1,12 @@
+# this file loads the json file and adds the values to the object.
+
+import json
+
+# Open the JSON file
+with open("../json/j8mb-icvb.json", "r") as f:
+    data = json.load(f)
+
+# Iterate over each dictionary in the list
+for dictionary in data:
+    # Print the items of each dictionary
+    print(dictionary.items())
